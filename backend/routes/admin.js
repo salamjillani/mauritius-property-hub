3c172779db2515d2a155d1e4dc4243b3636af030
Abdul Salam <92994347+abdulsalamshah@users.noreply.github.com>
2025-05-27 22:39:05 +0500

    commit

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,34 +1,50 @@
-// routes/admin.js
 const express = require('express');
 const router = express.Router();
-const {
-  getDashboard,
-  getUsers,
-  updateUser,
-  deleteUser,
-  getAgents,
-  getAgencies,
-  getPromoters,
-  getProperties,
-  approveProperty,
-  updateSubscription,
-  getLogs,
-} = require('../controllers/admin');
 const { protect, authorize } = require('../middleware/auth');
+const {
+  getSubscriptions,
+  getSubscription,
+  createSubscription,
+  updateSubscription,
+  featureProperty
+} = require('../controllers/subscriptions');
+const { getUsers, getUser, createUser, updateUser, deleteUser } = require('../controllers/users');
+const {
+  getProperties,
+  getProperty,
+  updateProperty,
+  deleteProperty
+} = require('../controllers/properties');
 
-router.use(protect);
-router.use(authorize('admin'));
+// Admin-only routes
+router.use(protect, authorize('admin'));
 
-router.get('/dashboard', getDashboard);
-router.get('/users', getUsers);
-router.put('/users/:id', updateUser);
-router.delete('/users/:id', deleteUser);
-router.get('/agents', getAgents);
-router.get('/agencies', getAgencies);
-router.get('/promoters', getPromoters);
-router.get('/properties', getProperties);
-router.put('/properties/:id/approve', approveProperty);
-router.put('/subscriptions/:id', updateSubscription);
-router.get('/logs', getLogs);
+router.route('/subscriptions')
+  .get(getSubscriptions)
+  .post(createSubscription);
+
+router.route('/subscriptions/:id')
+  .get(getSubscription)
+  .put(updateSubscription);
+
+router.route('/subscriptions/:id/feature-property')
+  .post(featureProperty);
+
+router.route('/users')
+  .get(getUsers)
+  .post(createUser);
+
+router.route('/users/:id')
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+router.route('/properties')
+  .get(getProperties);
+
+router.route('/properties/:id')
+  .get(getProperty)
+  .put(updateProperty)
+  .delete(deleteProperty);
 
 module.exports = router;
